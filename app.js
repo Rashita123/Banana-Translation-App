daa8257b32d0d036ce07d3de3bf553ed7400424d
Rashita123 <rrashita_be19@thapar.edu>
2020-12-03 17:12:24 +0530

    basic Javascript input output done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var btnTranslate= document.querySelector("#btn-translate");
+
+var btnTranslate=document.querySelector("#btn-translate");
+var output=document.querySelector(".output");
+var input=document.querySelector("#input-text");
 function clickEventHandler(){
-    console.log("Clicked!");
+    console.log(input.value);
+    output.innerText="ncjdn"+"for: "+input.value;
 }
-btnTranslate.addEventListener("click",  clickEventHandler);
+
+btnTranslate.addEventListener("click",clickEventHandler);
