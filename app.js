6305e2803aa4bc0907d13ca95cd93e6b623c686e
Rashita123 <rrashita_be19@thapar.edu>
2020-12-04 18:34:35 +0530

    JS done and added API link

--- app.js
+++ app.js
@@ -2,9 +2,26 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var output=document.querySelector(".output");
 var input=document.querySelector("#input-text");
+
+var serverURL="https://api.funtranslations.com/translate/braille/unicode.json";
+
+function URLConstruct(text){
+    return serverURL+"?"+"text="+text;
+}
+
+function errorHandler(error){
+    console.log("Error occured",error);
+    alert("Error Ocuured. Try after some time!");
+}
+
 function clickEventHandler(){
-    console.log(input.value);
-    output.innerText="ncjdn"+"for: "+input.value;
+    var text=input.value;
+    fetch(URLConstruct(text))
+    .then(response => response.json())
+    .then(json => {
+        output.innerText = json.contents.translated;
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click",clickEventHandler);
