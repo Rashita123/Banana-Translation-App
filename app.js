4564a3a551d267d08b5b748c9f30b05a4d8a8fc7
Rashita123 <rrashita_be19@thapar.edu>
2020-12-05 13:48:14 +0530

    fix: basic styling

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var output=document.querySelector(".output");
 var input=document.querySelector("#input-text");
 
 var brailleButton=document.querySelector("#braille-button")
-var signButton=document.querySelector("#sign-button")
 var minionButton=document.querySelector("#minion-button")
 
 var brailleURL="https://api.funtranslations.com/translate/braille/unicode.json";
@@ -23,11 +22,9 @@ function clickEventHandler(){
     var text=input.value;
     fetch(URLConstruct(text))
     .then(response => response.json())
-    .then(json => console.log(json));
-    // .then(json => {
-    //     output.innerText = json.contents.translated;
-    // })
-    console.log("helo");
+    .then(json => {
+        output.innerText = json.contents.translated;
+    })
     .catch(errorHandler)
 }
 
