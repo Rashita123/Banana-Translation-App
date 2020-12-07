9ab084ef8ef64bea39323a79d6308bf6a13867c7
Rashita123 <rrashita_be19@thapar.edu>
2020-12-07 14:54:05 +0530

    minor fix

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var btnTranslate=document.querySelector("#btn-translate");
 var output=document.querySelector(".output");
 var input=document.querySelector("#input-text");
 
-
+serverURL="https://api.funtranslations.com/translate/braille/unicode.json";
 function URLConstruct(text){
     return serverURL+"?"+"text="+text;
 }
 
-serverURL="https://api.funtranslations.com/translate/braille/unicode.json";
+
 
 function errorHandler(error){
     console.log("Error occured",error);
@@ -21,7 +21,8 @@ function clickEventHandler(){
     .then(json => {
         output.innerText = json.contents.translated;
     })
-    .catch(errorHandler)
+    // .then(json => console.log(json));
+    .catch(errorHandler);
 }
 
 
